1988700c3e83f62466117e796404c390dc3097da
Anthony Collette <100pour100inde@gmail.com>
2024-05-09 01:49:09 +0200

    add hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
 import './assets/scss/main.scss';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
 import Homepage from "./views/Homepage";
 import ContactPage from './views/ContactPage';
 import Mentions from './views/Mentions';
 import ScrollToTop from './components/ScrollToTop';
+import Error404 from './views/Error404';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <HashRouter>
       <ScrollToTop />
         <Routes>
-          
           <Route path="/" element={<Homepage />} />
           <Route path="/contact" element={<ContactPage />} />
           <Route path="/mentions-légales" element={<Mentions />} />
+          <Route path="*" element={<Error404 />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
