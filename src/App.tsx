e99828fe1852acbb06a177e386e2111b5dcb1d88
Anthony Collette <100pour100inde@gmail.com>
2024-05-09 01:39:46 +0200

    mentions

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Homepage from "./views/Homepage";
 import ContactPage from './views/ContactPage';
 import Mentions from './views/Mentions';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <ScrollToTop />
         <Routes>
+          
           <Route path="/" element={<Homepage />} />
           <Route path="/contact" element={<ContactPage />} />
           <Route path="/mentions-légales" element={<Mentions />} />
