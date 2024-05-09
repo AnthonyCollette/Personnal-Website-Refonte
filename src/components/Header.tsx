7c23fe6337960f5c917e5629b77aa85a04a401df
Anthony Collette <100pour100inde@gmail.com>
2024-05-09 02:04:56 +0200

    fix scroll

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,23 +10,23 @@ export default function Header() {
 
 
     return <header>
-            <div className="computer-header">
-                <img src={computer} alt="Ordinateur portable" className="computer-header__computer" />
-                <img src={screen} alt="Ecran d'ordinateur" className="computer-header__green-screen" />
-                <img src={displayingScreen} alt="Affichage d'écran d'ordinateur" className="computer-header__screen" /></div>
+        <div className="computer-header">
+            <img src={computer} alt="Ordinateur portable" className="computer-header__computer" />
+            <img src={screen} alt="Ecran d'ordinateur" className="computer-header__green-screen" />
+            <img src={displayingScreen} alt="Affichage d'écran d'ordinateur" className="computer-header__screen" /></div>
 
-            <Triangle />
-                <Container>
-                    <h1>
-                        anthony collette
-                    </h1>
-                    <p>
-                        Concevoir l&apos;<strong>interface</strong>, créer l&apos;<strong>expérience</strong>.
-                    </p>
-                    <p>Votre entreprise mérite une présence en ligne à la hauteur de son ambition. Je suis là pour concrétiser cette vision. <br />Passionné de développement et de design graphique, je vous accompagne dans chaque étape de <strong>votre transformation digitale</strong>. <br />De la refonte graphique de votre site web à l&apos;optimisation de son expérience utilisateur, je mets mon expertise au service de votre succès.</p>
-                    <button onClick={() => document.getElementById('about')?.scrollIntoView()} className="btn btn--primary-outline">
-                        Démarrez votre transformation digitale
-                    </button>
-                </Container>
+        <Triangle />
+        <Container>
+            <h1>
+                anthony collette
+            </h1>
+            <p>
+                Concevoir l&apos;<strong>interface</strong>, créer l&apos;<strong>expérience</strong>.
+            </p>
+            <p>Votre entreprise mérite une présence en ligne à la hauteur de son ambition. Je suis là pour concrétiser cette vision. <br />Passionné de développement et de design graphique, je vous accompagne dans chaque étape de <strong>votre transformation digitale</strong>. <br />De la refonte graphique de votre site web à l&apos;optimisation de son expérience utilisateur, je mets mon expertise au service de votre succès.</p>
+            <button onClick={() => document.getElementById('about')?.scrollIntoView()} className="btn btn--primary-outline">
+                Démarrez votre transformation digitale
+            </button>
+        </Container>
     </header>
 }
